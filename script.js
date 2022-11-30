167f059cdac2e32b1ac92369df4511a93c628906
jms <jamesg9612@gmail.com>
2022-11-30 08:02:04 -0500

    dfg

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-let button = document.getElementsByName("random button")
-let change = document.getElementById("change")
 let h1 = document.getElementsByName("heading")
+let maxnum = document.getElementsByName("maxnum")
+let label = document.getElementById("label")
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 function showAns(){
-    var randnum = getRandomInt(109320)
-    h1.innerHTML = randnum.toString()
-    console.log("devious")
+    var randnum = getRandomInt(maxnum[0].value)
+    h1[0].innerHTML = "✼" + randnum + "✼"
+}
+
+function disappear(){
+    for(i = 0; i++; i < 50){
+        label.style.opacity -= 2
+    }
 }
